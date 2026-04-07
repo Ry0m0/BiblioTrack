263c51bd25538811aac4fbb9126024368b095af7
sghiarabdellah-svg <sghiarabdellah@gmail.com>
2026-04-07 09:57:34 +0200

    Modify main method to show console menu
    Updated main method to launch console menu instead of GUI.

--- src/main/java/com/biblio/consola/main.java
+++ src/main/java/com/biblio/consola/main.java
@@ -3,7 +3,9 @@ package com.biblio;
 import com.biblio.gui.VentanaPrincipal;
 
 public class main {
-public static void main(String[] args) {
-new VentanaPrincipal().setVisible(true);
-}
+    public static void main(String[] args) {
+        // Create the console version and start the menu
+        VentanaPrincipal vp = new VentanaPrincipal();
+        vp.mostrar(); // this launches the console menu
+    }
 }
