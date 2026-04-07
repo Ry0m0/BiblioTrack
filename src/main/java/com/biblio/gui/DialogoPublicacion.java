e5366ae09a2adcf0d667da1a392f20a36e53a89a
sghiarabdellah-svg <sghiarabdellah@gmail.com>
2026-04-07 09:56:46 +0200

    Update DialogoPublicacion.java

--- src/main/java/com/biblio/gui/DialogoPublicacion.java
+++ src/main/java/com/biblio/gui/DialogoPublicacion.java
@@ -1,14 +1,6 @@
 package com.biblio.gui;
 
-import java.awt.GridLayout;
-
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JTextField;
+import java.util.Scanner;
 
 import com.biblio.modelo.Comic;
 import com.biblio.modelo.Genero;
@@ -16,87 +8,37 @@ import com.biblio.modelo.LibroTecnico;
 import com.biblio.modelo.Novela;
 import com.biblio.modelo.Publicacion;
 
-public class DialogoPublicacion extends JDialog {
+public class DialogoPublicacion {
 
+    private Scanner sc;
 
-private JTextField txtTitulo;
-private JTextField txtAutor;
-private JTextField txtAnio;
-private JComboBox<String> comboTipo;
+    public DialogoPublicacion(Scanner sc) {
+        this.sc = sc;
+    }
 
-private Publicacion creada;
+    public Publicacion crearPublicacion() {
+        System.out.print("Título: ");
+        String titulo = sc.nextLine();
+        System.out.print("Autor: ");
+        String autor = sc.nextLine();
+        System.out.print("Año: ");
+        int anio = Integer.parseInt(sc.nextLine());
 
-public DialogoPublicacion(JFrame parent) {
-    super(parent, "Nueva publicación", true);
-    setSize(400, 250);
-    setLocationRelativeTo(parent);
-    setLayout(new GridLayout(5, 2));
-
-    add(new JLabel("Título:"));
-    txtTitulo = new JTextField();
-    add(txtTitulo);
-
-    add(new JLabel("Autor:"));
-    txtAutor = new JTextField();
-    add(txtAutor);
-
-    add(new JLabel("Año:"));
-    txtAnio = new JTextField();
-    add(txtAnio);
-
-    add(new JLabel("Tipo:"));
-    comboTipo = new JComboBox<>(new String[]{"Comic", "Novela", "Tecnico"});
-    add(comboTipo);
-
-    JButton btnCrear = new JButton("Crear");
-    btnCrear.addActionListener(e -> crear());
-
-    add(new JLabel());
-    add(btnCrear);
-}
-
-private void crear() {
-    try {
-        String titulo = txtTitulo.getText();
-        String autor = txtAutor.getText();
-        int anio = Integer.parseInt(txtAnio.getText());
+        System.out.print("Tipo (Comic, Novela, Tecnico): ");
+        String tipo = sc.nextLine();
 
         String[] etiquetas = {"general"};
 
-        String tipo = comboTipo.getSelectedItem().toString();
-
-        switch (tipo) {
-            case "Comic":
-                creada = new Comic(titulo, autor, anio, Genero.COMIC, etiquetas);
-                break;
-
-            case "Novela":
-                creada = new Novela(titulo, autor, anio, Genero.NOVELA, etiquetas);
-                break;
-
-            case "Tecnico":
-                creada = new LibroTecnico(
-                        titulo,
-                        autor,
-                        anio,
-                        Genero.TECNICO,
-                        etiquetas,
-                        1,
-                        "General"
-                );
-                break;
+        switch (tipo.toLowerCase()) {
+            case "comic":
+                return new Comic(titulo, autor, anio, Genero.COMIC, etiquetas);
+            case "novela":
+                return new Novela(titulo, autor, anio, Genero.NOVELA, etiquetas);
+            case "tecnico":
+                return new LibroTecnico(titulo, autor, anio, Genero.TECNICO, etiquetas, 1, "General");
+            default:
+                System.out.println("Tipo inválido");
+                return null;
         }
-
-        dispose();
-
-    } catch (Exception e) {
-        JOptionPane.showMessageDialog(this, "Error: " + e.getMessage());
     }
-}
-
-public Publicacion getPublicacion() {
-    return creada;
-}
-
-
 }
