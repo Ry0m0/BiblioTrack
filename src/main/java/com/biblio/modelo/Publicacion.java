c1692c3c4667c5a1b0e0ebc1e1e2ce492fa8f3ad
sghiarabdellah-svg <sghiarabdellah@gmail.com>
2026-04-07 08:55:32 +0200

    Create abstract Publicacion class with properties and methods

--- src/main/java/com/biblio/modelo/Publicacion.java
+++ src/main/java/com/biblio/modelo/Publicacion.java
@@ -1,3 +1,43 @@
 package com.biblio.modelo;
 
 import java.util.UUID;
+public abstract class Publicacion {
+
+```
+protected String titulo;
+protected String autor;
+protected int anioPublicacion;
+protected Genero genero;
+
+public Publicacion(String titulo, String autor, int anioPublicacion, Genero genero) {
+    this.titulo = titulo;
+    this.autor = autor;
+    this.anioPublicacion = anioPublicacion;
+    this.genero = genero;
+}
+
+public String getTitulo() {
+    return titulo;
+}
+
+public String getAutor() {
+    return autor;
+}
+
+public int getAnioPublicacion() {
+    return anioPublicacion;
+}
+
+public Genero getGenero() {
+    return genero;
+}
+
+public void mostrarInfo() {
+    System.out.println("Título: " + titulo);
+    System.out.println("Autor: " + autor);
+    System.out.println("Año: " + anioPublicacion);
+    System.out.println("Género: " + genero);
+}
+```
+
+}
